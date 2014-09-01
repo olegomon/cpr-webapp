0deaea539ef29a5ad501160425bc921d8d19968c
Oleg Buss <oleg.buss@canoo.com>
2014-09-01 17:24:59 +0200

    some markup simplifications, added demo route to demonstrate computer vs. computer mode, added winner animation

--- src/app/engine/engine.constants.js
+++ src/app/engine/engine.constants.js
@@ -1,10 +1,4 @@
 angular.module('cpr.engine.constants', [
 ])
-    .constant('GESTURES', {
-        ROCK    : 0,
-        PAPER   : 1,
-        SCISSORS: 2,
-        LIZARD  : 3,
-        SPOCK   : 4
-    })
+    
 ;
